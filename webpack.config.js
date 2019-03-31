0ce650f8a593ea3653784d5228c6379efabe8eb6
Sobak <msobaczewski@gmail.com>
2019-03-31 05:04:19 +0200

    Setup basic theme

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel(() => {}, {
+        useBuiltIns: 'usage',
+        corejs: 3
+    })
+
+
+    .enableSassLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
